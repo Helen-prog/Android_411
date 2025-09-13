91393b3f96fca4931ec44788ca6d25858e687faa
Helen <kozovyakina@gmail.com>
2025-09-13 13:03:29 +0300

    retrofit

--- RetrofitApp/app/src/main/java/com/example/retrofitapp/JSONPlaceholder.java
+++ RetrofitApp/app/src/main/java/com/example/retrofitapp/JSONPlaceholder.java
@@ -4,9 +4,32 @@ import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
 
 public interface JSONPlaceholder {
 
     @GET("posts")
     Call<List<Post>> getPost();
+
+//    @GET("posts/{id}/comments")
+//    Call<List<Comment>> getComments(@Path("id") int postId);
+
+    @GET("comments")
+    Call<List<Comment>> getComments(@Query("postId") int postId);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- RetrofitApp/app/src/main/java/com/example/retrofitapp/MainActivity.java
+++ RetrofitApp/app/src/main/java/com/example/retrofitapp/MainActivity.java
@@ -1,24 +1,91 @@
 package com.example.retrofitapp;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    JSONPlaceholder jsonPlaceholder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://jsonplaceholder.typicode.com/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        jsonPlaceholder = retrofit.create(JSONPlaceholder.class);
+
+//        getPost();
+        getComments();
+
+    }
+
+    private void getComments() {
+        Call<List<Comment>> call = jsonPlaceholder.getComments(4);
+
+        call.enqueue(new Callback<List<Comment>>() {
+            @Override
+            public void onResponse(Call<List<Comment>> call, Response<List<Comment>> response) {
+                if(!response.isSuccessful()){
+                    Toast.makeText(MainActivity.this, response.code(), Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                List<Comment> comments = response.body();
+                CommentAdapter commentAdapter = new CommentAdapter(MainActivity.this, comments);
+                recyclerView.setAdapter(commentAdapter);
+            }
+
+            @Override
+            public void onFailure(Call<List<Comment>> call, Throwable t) {
+                Toast.makeText(MainActivity.this, t.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+
+    public void getPost(){
+        Call<List<Post>> call = jsonPlaceholder.getPost();
+
+        call.enqueue(new Callback<List<Post>>() {
+            @Override
+            public void onResponse(Call<List<Post>> call, Response<List<Post>> response) {
+                if (!response.isSuccessful()) {
+                    Toast.makeText(MainActivity.this, response.code(), Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                List<Post> postList = response.body();
+                PostAdapter postAdapter = new PostAdapter(MainActivity.this, postList);
+                recyclerView.setAdapter(postAdapter);
+            }
+
+            @Override
+            public void onFailure(Call<List<Post>> call, Throwable t) {
+                Toast.makeText(MainActivity.this, t.getMessage(), Toast.LENGTH_SHORT).show();
+            }
         });
     }
 }
